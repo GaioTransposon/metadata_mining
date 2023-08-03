bb99ffb02f8968dbc0a1d074d17834f9597d6bfb
GaioTransposon <gaio.transposon@gmail.com>
2023-08-03 17:26:38 +0200

    extract pmids now all comparable to shell

--- scripts/extract_pmids.py
+++ scripts/extract_pmids.py
@@ -21,7 +21,7 @@ import argparse
 
 def find_pmids_from_large_file(file_path):
     pmid_dict = {}
-    pmid_pattern = re.compile(r"(PMID|pmid)\D*(\d{8})", re.IGNORECASE)
+    pmid_pattern = re.compile(r"(PMID|pmid)\D*([\d]+)", re.IGNORECASE)
     
     start_time = time.time()
     sample_counter = 0
@@ -41,9 +41,9 @@ def find_pmids_from_large_file(file_path):
                 print(f"Processed {sample_counter} samples in {elapsed_time:.2f} seconds "  
                       f"({sample_counter / elapsed_time:.2f} samples/second)")
         else:
-            match = pmid_pattern.search(line)
-            if match:
-                pmid_dict[sample_name].append(match.group())
+            matches = pmid_pattern.findall(line)
+            for match in matches:
+                pmid_dict[sample_name].append(match[1])  # get only the digits of PMID
 
     return pmid_dict
 
@@ -57,16 +57,22 @@ def dict_to_csv(dictionary, filename):
             if values:
                 pmid = '&'.join(values)
                 pmid_digits = re.findall(r'\d+', pmid)
-                first_digits = pmid_digits[0] if pmid_digits else ''
-                writer.writerow([key, pmid, first_digits])
+                all_digits = ';'.join(pmid_digits) if pmid_digits else ''
+                writer.writerow([key, pmid, all_digits])
             else:
                 writer.writerow([key, '', ''])
 
-                
+
 def plot_pmid_info(filename, figure_path=None):
     data = pd.read_csv(filename)
-    non_na_count = data['pmid_digits'].notna().sum()
-    na_count = data['pmid_digits'].isna().sum()
+    # Creating a new column that indicates whether pmid_digits is NA or not
+    data['has_pmid'] = data['pmid_digits'].notna()
+
+    # Group by sample and aggregate by max, which ensures at most one PMID is counted per sample
+    data_grouped = data.groupby('sample').agg({'has_pmid': 'max'})
+    
+    non_na_count = data_grouped['has_pmid'].sum()
+    na_count = len(data_grouped) - non_na_count
 
     labels = ['Non-NA', 'NA']
     counts = [non_na_count, na_count]
@@ -74,7 +80,7 @@ def plot_pmid_info(filename, figure_path=None):
     plt.bar(labels, counts)
     plt.xlabel('Column "pmid_digits"')
     plt.ylabel('Count')
-    plt.title('Count of Non-NA Values in Column "pmid_digits"')
+    plt.title('Count of Non-NA Values in Column "pmid_digits" - (counting max 1 pmid per sample)')
 
     for i, count in enumerate(counts):
         plt.text(i, count, str(count), ha='center', va='bottom')
@@ -83,6 +89,26 @@ def plot_pmid_info(filename, figure_path=None):
         plt.savefig(figure_path, format='pdf')
     else:
         plt.show()
+
+
+
+def compare_with_shell_output(python_dict, shell_file):
+    # Load shell output into a pandas Series
+    shell_output = pd.read_csv(shell_file, header=None).squeeze("columns").astype(str)
+    
+    # Flatten python_dict values into a list
+    python_output = [pmid for pmids in python_dict.values() for pmid in pmids]
+    python_output = pd.Series(python_output)
+    
+    # Compare
+    not_in_python = shell_output.loc[~shell_output.isin(python_output)]
+    not_in_shell = python_output.loc[~python_output.isin(shell_output)]
+    
+    
+    print("PMIDs found in shell output but not in Python output:\n", not_in_python)
+    print("PMIDs found in Python output but not in shell output:\n", not_in_shell)
+
+
 
 
 parser = argparse.ArgumentParser(description='Find PMIDs in the large file.')
@@ -104,53 +130,30 @@ if args.plot:
 
 
 
-
-
-
-
-
-
-
-# =============================================================================
-# import pandas as pd
-# import numpy as np
-# 
-# def find_pmids_from_large_file(file_path):
-#     pmid_dict = {}
-#     pmid_pattern = re.compile(r"(PMID|pmid)\D*(\d+)", re.IGNORECASE)
-#     
-#     with open(file_path, 'r') as f:
-#         lines = f.readlines()
-# 
-#     sample_name = ''
-#     for line in lines:
-#         if line.startswith('>'):
-#             sample_name = line.replace('>', '').strip()
-#             pmid_dict[sample_name] = []
-#         else:
-#             match = pmid_pattern.search(line)
-#             if match:
-#                 pmid_dict[sample_name].append(match.group(2))  # get only the digits of PMID
-#                 
-#     return pmid_dict
-# 
-# def compare_with_shell_output(python_dict, shell_file):
-#     # Load shell output into a pandas Series
-#     shell_output = pd.read_csv(shell_file, header=None, squeeze=True)
-#     
-#     # Flatten python_dict values into a list
-#     python_output = [pmid for pmids in python_dict.values() for pmid in pmids]
-#     python_output = pd.Series(python_output)
-#     
-#     # Compare
-#     not_in_python = shell_output.loc[~shell_output.isin(python_output)]
-#     not_in_shell = python_output.loc[~python_output.isin(shell_output)]
-#     
-#     print("PMIDs found in shell output but not in Python output:\n", not_in_python)
-#     print("PMIDs found in Python output but not in shell output:\n", not_in_shell)
-# 
-# # Usage
-# pmid_dict = find_pmids_from_large_file("/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/sample.info")
+# if you want to compare the pmid extraction with one obtained using shell as such:
+# grep -i -o -E 'PMID[^0-9]*[0-9]+' sample.info | grep -o -E '[0-9]+' > get_pmids_test.txt
+# run: 
 # compare_with_shell_output(pmid_dict, "/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/get_pmids_test.txt")
-# =============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
