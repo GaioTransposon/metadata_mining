c6fea43e1b953d825c18b02ad8eef65b9918a472
GaioTransposon <gaio.transposon@gmail.com>
2023-08-23 19:14:18 +0200

    get metadata with entrez. finally

---
+++ scripts/get_ncbi_metadata_entrez.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 17:00:54 2023
+
+@author: dgaio
+"""
+
+
+
+# # run as: 
+# python ~/github/metadata_mining/scripts/get_ncbi_metadata_entrez.py  \
+#         --work_dir '/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/' \
+#             --sample_info_biome_pmid "sample.info_biome_pmid.csv" \
+#                 --output_file "sample.info_biome_pmid_title_abstract.csv" \
+#                         --figure "sample.info_biome_pmid_title_abstract.pdf"
+    
+
+import pandas as pd
+import os
+import gzip
+import argparse  
+import ast
+import xml.etree.ElementTree as ET
+import time
+import pickle
+import matplotlib.pyplot as plt
+import requests
+from xml.etree import ElementTree as ET
+import time
+import pandas as pd
+
+
+####################
+
+parser = argparse.ArgumentParser(description='Process XML files.')
+
+parser.add_argument('--work_dir', type=str, required=True, help='path to work directory')
+parser.add_argument('--sample_info_biome_pmid', type=str, required=True, help='path to input df')
+parser.add_argument('--output_file', type=str, required=True, help='name of output file')
+parser.add_argument('--figure', type=str, required=True, help='name of figure file')
+
+args = parser.parse_args()
+
+# Prepend work_dir to all the file paths
+sample_info_biome_pmid = os.path.join(args.work_dir, args.sample_info_biome_pmid)
+output_file = os.path.join(args.work_dir, args.output_file)
+figure = os.path.join(args.work_dir, args.figure)
+
+
+# # for testing purposes
+# work_dir = "/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/"
+# sample_info_biome_pmid = os.path.join(work_dir, "sample.info_biome_pmid.csv")
+# output_file = os.path.join(work_dir, "ncbi_metadata_selection2.csv")
+# figure = os.path.join(work_dir, "ncbi_metadata_selection2.pdf")
+# ###################
+
+
+############ 1. open input df
+s = pd.read_csv(sample_info_biome_pmid)
+
+############ 2. get a list of unique PMIDs
+
+# Convert string representation of lists to actual lists
+s['pmid'] = s['pmid'].apply(lambda x: ast.literal_eval(x) if pd.notna(x) else x)
+
+# Flatten the lists and get unique PMIDs
+unique_pmids = pd.Series([pmid for sublist in s['pmid'].dropna() for pmid in sublist]).unique().tolist()
+len(unique_pmids)
+type(unique_pmids)
+
+############ 3. get info from Entrez 
+
+
+
+def extract_all_text(element):
+    if element is None:
+        return None
+    
+    texts = []
+    for t in element.itertext():
+        if t:
+            texts.append(t.strip())
+    return "".join(texts).strip()
+
+
+
+def fetch_articles(pmids):
+    base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+    
+    df = pd.DataFrame(columns=["pmid", "title", "abstract"])
+    pmids_no_title = []
+    pmids_no_abstract = []
+    pmids_no_title_no_abstract = []
+
+    # Splitting the list of pmids into batches of 200
+    for i in range(0, len(pmids), 200):
+        batch_pmids = pmids[i:i+200]
+        params = {
+            "db": "pubmed",
+            "id": ",".join(batch_pmids),
+            "retmode": "xml"
+        }
+        response = requests.get(base_url, params=params)
+        root = ET.fromstring(response.content)
+
+        new_rows = []
+        for article in root.findall(".//PubmedArticle"):
+            pmid = article.find(".//PMID").text
+            title_element = article.find(".//ArticleTitle")
+            abstract_element = article.find(".//AbstractText")
+
+            #title = title_element.text.strip() if title_element is not None and title_element.text and title_element.text.strip() != "" else None
+            #abstract = abstract_element.text.strip() if abstract_element is not None and abstract_element.text and abstract_element.text.strip() != "" else None
+
+            title = extract_all_text(title_element)
+            abstract = extract_all_text(abstract_element)
+
+
+            #title = title_element.text if title_element is not None else None
+            #abstract = abstract_element.text if abstract_element is not None else None
+
+            # Checking if title and/or abstract are missing and populating the lists
+            if title is None and abstract is not None:
+                pmids_no_title.append(pmid)
+            elif title is not None and abstract is None:
+                pmids_no_abstract.append(pmid)
+            elif title is None and abstract is None:
+                pmids_no_title_no_abstract.append(pmid)
+
+            new_rows.append({"pmid": pmid, "title": title, "abstract": abstract})
+
+        df = pd.concat([df, pd.DataFrame(new_rows)], ignore_index=True)
+
+        # Pausing for 3 seconds between requests and print progress
+        print(f"Processed {i + len(batch_pmids)} out of {len(pmids)} PMIDs...")
+        time.sleep(3)
+
+    print(f"\nPMIDs with no title: {len(pmids_no_title)}")
+    print(f"PMIDs with no abstract: {len(pmids_no_abstract)}")
+    print(f"PMIDs with both missing: {len(pmids_no_title_no_abstract)}")
+
+    return df, pmids_no_title, pmids_no_abstract, pmids_no_title_no_abstract
+
+
+
+
+# Sample PMIDs
+pmids_sample = unique_pmids # ["30074988", "31651534", "31661069", "33004443",  "34566912", "29359185", "32595912"]
+
+start_time = time.time()
+df_retrieved, pmids_no_title, pmids_no_abstract, pmids_no_title_no_abstract = fetch_articles(pmids_sample)
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+articles_per_second = len(df_retrieved) / elapsed_time
+
+print(f"\nTime taken: {elapsed_time:.2f} seconds")
+print(f"Rate: {articles_per_second:.2f} articles per second")
+
+print(f"PMIDs processed: {len(df_retrieved)}")
+print(f"PMIDs without title: {len(pmids_no_title)}")
+print(f"PMIDs without abstract: {len(pmids_no_abstract)}")
+print(f"PPMIDs without title and abstract: {len(pmids_no_title_no_abstract)}")
+
+
+
+
+############ 4. Merge ncbi metadata to our original dataframe:  
+
+
+# Step 1: Expand the 's' dataframe
+s_exploded = s.explode('pmid')
+
+# Step 2: Merge with 'df_accommodate_metadata'
+merged_df = s_exploded.merge(df_retrieved, on='pmid', how='left')
+
+# Step 3: Post-process (if needed)
+merged_df = merged_df[['sample', 'biome', 'pmid', 'title', 'abstract']]  # Reorder columns
+
+# Save
+merged_df.to_csv(os.path.join(output_file), index=False)
+print("Output file succesfully written")
+
+
+
+############ 5. Plot: 
+    
+
+# Filtering out rows with None in title or abstract
+filtered_df = merged_df.dropna(subset=['title', 'abstract'])
+
+# Grouping by biome and sample to get the unique pmids count
+grouped = filtered_df.groupby(['biome', 'sample']).agg({
+    'pmid': 'nunique'
+}).reset_index()
+
+# Adding the has_min_1_t+a column
+grouped['has_min_1_t+a'] = (grouped['pmid'] >= 1).astype(int)
+
+# Aggregating again on biome
+biome_summary = grouped.groupby('biome').agg({
+    'sample': 'count',
+    'has_min_1_t+a': 'sum'
+}).reset_index()
+
+
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Using Seaborn for the bar plot
+plt.figure(figsize=(12, 8))
+plot = sns.barplot(data=biome_summary, x='biome', y='has_min_1_t+a', palette="viridis")
+
+# Adding the count annotations on top of the bars
+for p in plot.patches:
+    plot.annotate(f'{int(p.get_height())}',
+                  (p.get_x() + p.get_width() / 2., p.get_height()),
+                  ha='center', va='center', 
+                  xytext=(0, 9),
+                  textcoords='offset points')
+
+plt.title('Number of samples with at least one title and abstract per biome')
+plt.ylabel('Count of samples')
+plt.xlabel('Biome')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+
+
+# 1. Filter out rows where the title or abstract is NaN
+filtered_df = merged_df.dropna(subset=['title', 'abstract'])
+
+# 2. For each biome, count the number of samples with at least one associated PMID
+sample_counts = filtered_df.groupby('biome')['sample'].nunique().reset_index().rename(columns={'sample': 'samples_with_t+a'})
+
+# 3. Count the number of unique PMIDs per biome
+pmid_counts = filtered_df.groupby('biome')['pmid'].nunique().reset_index().rename(columns={'pmid': 'unique_pmids'})
+
+# Merge the two dataframes
+biome_summary = sample_counts.merge(pmid_counts, on='biome')
+
+# 4. Plot
+plt.figure(figsize=(12, 8))
+
+# First, plot the samples_with_t+a bars
+bars1 = plt.bar(biome_summary['biome'], biome_summary['samples_with_t+a'], label='Samples with Title & Abstract', color='b')
+
+# Then, plot the unique_pmids as a stacked bar on top
+bars2 = plt.bar(biome_summary['biome'], biome_summary['unique_pmids'], bottom=biome_summary['samples_with_t+a'], label='Unique PMIDs', color='r')
+
+# Add annotations on top of the bars
+for bar in bars1:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/2, yval + 5, round(yval, 2), ha="center", va="bottom")
+
+for bar in bars2:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/2, yval + bar.get_y() + 5, round(yval, 2), ha="center", va="bottom")
+
+plt.title('Comparison of samples with title & abstract vs unique PMIDs per biome')
+plt.ylabel('Count')
+plt.xlabel('Biome')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.legend(title="Metrics")
+plt.show()
+
+
+plt.figure(figsize=(15, 10))  # Increase figure size
+
+# First, plot the samples_with_t+a bars
+bars1 = plt.bar(biome_summary['biome'], biome_summary['samples_with_t+a'], label='Samples with Title & Abstract', color='b')
+
+# Then, plot the unique_pmids as a stacked bar on top
+bars2 = plt.bar(biome_summary['biome'], biome_summary['unique_pmids'], bottom=biome_summary['samples_with_t+a'], label='Unique PMIDs', color='r')
+
+# Add annotations on top of the bars with adjusted position and reduced font size
+for bar in bars1:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/4, yval + 0.5, round(yval, 2), ha="center", va="bottom", fontsize=10, color='b')
+
+for bar in bars2:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + 3*bar.get_width()/4, yval + bar.get_y() + 0.5, round(yval, 2), ha="center", va="bottom", fontsize=10, color='r')
+
+plt.title('Comparison of samples with title & abstract vs unique PMIDs per biome')
+plt.ylabel('Count')
+plt.xlabel('Biome')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.legend(title="Metrics")
+plt.show()
+
+
+
+
+
+
