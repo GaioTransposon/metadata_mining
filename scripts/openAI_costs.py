1dc63a98fac2a07822d1ced8ba323a5f1830a92e
GaioTransposon <gaio.transposon@gmail.com>
2023-10-24 15:09:18 +0200

    envo_translate.py

--- scripts/openAI_costs.py
+++ scripts/openAI_costs.py
@@ -9,106 +9,110 @@ Created on Mon Oct 23 13:10:03 2023
 import re
 
 text = """
-2:00 PM
-Local time: Oct 20, 2023, 4:00 PM
-gpt-3.5-turbo-16k-0613, 13 requests
-20,387 prompt + 485 completion = 20,872 tokens
-2:10 PM
-Local time: Oct 20, 2023, 4:10 PM
-gpt-3.5-turbo-16k-0613, 42 requests
-69,300 prompt + 1,769 completion = 71,069 tokens
-2:15 PM
-Local time: Oct 20, 2023, 4:15 PM
-gpt-3.5-turbo-16k-0613, 40 requests
-64,601 prompt + 1,522 completion = 66,123 tokens
-2:25 PM
-Local time: Oct 20, 2023, 4:25 PM
-gpt-3.5-turbo-16k-0613, 42 requests
-72,539 prompt + 2,094 completion = 74,633 tokens
-2:30 PM
-Local time: Oct 20, 2023, 4:30 PM
+11:00 AM
+Local time: Oct 23, 2023, 1:00 PM
+gpt-3.5-turbo-16k-0613, 28 requests
+76,788 prompt + 2,246 completion = 79,034 tokens
+11:05 AM
+Local time: Oct 23, 2023, 1:05 PM
 gpt-3.5-turbo-16k-0613, 44 requests
-71,858 prompt + 1,788 completion = 73,646 tokens
-2:35 PM
-Local time: Oct 20, 2023, 4:35 PM
-gpt-3.5-turbo-16k-0613, 55 requests
-122,529 prompt + 3,165 completion = 125,694 tokens
-2:40 PM
-Local time: Oct 20, 2023, 4:40 PM
-gpt-3.5-turbo-16k-0613, 51 requests
-117,889 prompt + 2,826 completion = 120,715 tokens
-2:45 PM
-Local time: Oct 20, 2023, 4:45 PM
-gpt-3.5-turbo-16k-0613, 64 requests
-112,003 prompt + 3,192 completion = 115,195 tokens
-2:50 PM
-Local time: Oct 20, 2023, 4:50 PM
-gpt-3.5-turbo-16k-0613, 116 requests
-138,190 prompt + 3,479 completion = 141,669 tokens
-2:55 PM
-Local time: Oct 20, 2023, 4:55 PM
-gpt-3.5-turbo-16k-0613, 8 requests
-10,567 prompt + 270 completion = 10,837 tokens
-3:00 PM
-530 requests
-3:05 PM
-Local time: Oct 20, 2023, 5:05 PM
-gpt-3.5-turbo-16k-0613, 80 requests
-116,000 prompt + 2,914 completion = 118,914 tokens
-3:10 PM
-Local time: Oct 20, 2023, 5:10 PM
-gpt-3.5-turbo-16k-0613, 34 requests
-113,460 prompt + 2,656 completion = 116,116 tokens
-3:15 PM
-Local time: Oct 20, 2023, 5:15 PM
-gpt-3.5-turbo-16k-0613, 34 requests
-117,338 prompt + 2,680 completion = 120,018 tokens
-3:20 PM
-Local time: Oct 20, 2023, 5:20 PM
-gpt-3.5-turbo-16k-0613, 15 requests
-47,483 prompt + 1,254 completion = 48,737 tokens
-3:30 PM
-Local time: Oct 20, 2023, 5:30 PM
-gpt-3.5-turbo-16k-0613, 200 requests
-154,188 prompt + 4,212 completion = 158,400 tokens
-3:35 PM
-Local time: Oct 20, 2023, 5:35 PM
-gpt-3.5-turbo-16k-0613, 102 requests
-87,181 prompt + 3,065 completion = 90,246 tokens
-3:40 PM
-Local time: Oct 20, 2023, 5:40 PM
-gpt-3.5-turbo-16k-0613, 22 requests
-110,335 prompt + 2,804 completion = 113,139 tokens
-3:45 PM
-Local time: Oct 20, 2023, 5:45 PM
-gpt-3.5-turbo-16k-0613, 23 requests
-117,452 prompt + 2,895 completion = 120,347 tokens
-3:50 PM
-Local time: Oct 20, 2023, 5:50 PM
-gpt-3.5-turbo-16k-0613, 14 requests
-72,735 prompt + 2,021 completion = 74,756 tokens
-3:55 PM
-Local time: Oct 20, 2023, 5:55 PM
-gpt-3.5-turbo-16k-0613, 6 requests
-42,350 prompt + 1,092 completion = 43,442 tokens
-4:00 PM
-35 requests
-4:00 PM
-Local time: Oct 20, 2023, 6:00 PM
-gpt-3.5-turbo-16k-0613, 16 requests
-115,144 prompt + 2,639 completion = 117,783 tokens
-4:05 PM
-Local time: Oct 20, 2023, 6:05 PM
-gpt-3.5-turbo-16k-0613, 8 requests
-65,977 prompt + 1,430 completion = 67,407 tokens
-4:15 PM
-Local time: Oct 20, 2023, 6:15 PM
-gpt-3.5-turbo-16k-0613, 9 requests
-65,765 prompt + 1,669 completion = 67,434 tokens
-4:20 PM
-Local time: Oct 20, 2023, 6:20 PM
+120,626 prompt + 2,753 completion = 123,379 tokens
+11:10 AM
+Local time: Oct 23, 2023, 1:10 PM
+gpt-3.5-turbo-16k-0613, 42 requests
+114,664 prompt + 2,904 completion = 117,568 tokens
+11:15 AM
+Local time: Oct 23, 2023, 1:15 PM
+gpt-3.5-turbo-16k-0613, 13 requests
+35,935 prompt + 958 completion = 36,893 tokens
+11:20 AM
+Local time: Oct 23, 2023, 1:20 PM
+gpt-3.5-turbo-16k-0613, 46 requests
+121,436 prompt + 3,090 completion = 124,526 tokens
+11:25 AM
+Local time: Oct 23, 2023, 1:25 PM
 gpt-3.5-turbo-16k-0613, 2 requests
-12,692 prompt + 310 completion = 13,002 tokens
+4,787 prompt + 54 completion = 4,841 tokens
+11:35 AM
+Local time: Oct 23, 2023, 1:35 PM
+gpt-3.5-turbo-16k-0613, 44 requests
+125,329 prompt + 3,054 completion = 128,383 tokens
+11:40 AM
+Local time: Oct 23, 2023, 1:40 PM
+gpt-3.5-turbo-16k-0613, 26 requests
+71,920 prompt + 1,947 completion = 73,867 tokens
+11:45 AM
+Local time: Oct 23, 2023, 1:45 PM
+gpt-3.5-turbo-16k-0613, 44 requests
+114,829 prompt + 2,776 completion = 117,605 tokens
+11:50 AM
+Local time: Oct 23, 2023, 1:50 PM
+gpt-3.5-turbo-16k-0613, 43 requests
+123,343 prompt + 2,959 completion = 126,302 tokens
+11:55 AM
+Local time: Oct 23, 2023, 1:55 PM
+gpt-3.5-turbo-16k-0613, 43 requests
+117,712 prompt + 2,838 completion = 120,550 tokens
+12:00 PM
+434 requests
+12:00 PM
+Local time: Oct 23, 2023, 2:00 PM
+gpt-3.5-turbo-16k-0613, 41 requests
+113,567 prompt + 2,577 completion = 116,144 tokens
+12:05 PM
+Local time: Oct 23, 2023, 2:05 PM
+gpt-3.5-turbo-16k-0613, 39 requests
+104,608 prompt + 2,707 completion = 107,315 tokens
+12:10 PM
+Local time: Oct 23, 2023, 2:10 PM
+gpt-3.5-turbo-16k-0613, 44 requests
+119,383 prompt + 3,195 completion = 122,578 tokens
+12:15 PM
+Local time: Oct 23, 2023, 2:15 PM
+gpt-3.5-turbo-16k-0613, 43 requests
+121,550 prompt + 2,701 completion = 124,251 tokens
+12:20 PM
+Local time: Oct 23, 2023, 2:20 PM
+gpt-3.5-turbo-16k-0613, 45 requests
+121,242 prompt + 3,011 completion = 124,253 tokens
+12:25 PM
+Local time: Oct 23, 2023, 2:25 PM
+gpt-3.5-turbo-16k-0613, 44 requests
+114,829 prompt + 2,693 completion = 117,522 tokens
+12:30 PM
+Local time: Oct 23, 2023, 2:30 PM
+gpt-3.5-turbo-16k-0613, 43 requests
+123,343 prompt + 3,064 completion = 126,407 tokens
+12:35 PM
+Local time: Oct 23, 2023, 2:35 PM
+gpt-3.5-turbo-16k-0613, 21 requests
+55,659 prompt + 1,431 completion = 57,090 tokens
+12:45 PM
+Local time: Oct 23, 2023, 2:45 PM
+gpt-3.5-turbo-16k-0613, 28 requests
+76,151 prompt + 1,946 completion = 78,097 tokens
+12:50 PM
+Local time: Oct 23, 2023, 2:50 PM
+gpt-3.5-turbo-16k-0613, 45 requests
+126,193 prompt + 2,836 completion = 129,029 tokens
+12:55 PM
+Local time: Oct 23, 2023, 2:55 PM
+gpt-3.5-turbo-16k-0613, 41 requests
+109,734 prompt + 2,732 completion = 112,466 tokens
+1:00 PM
+101 requests
+1:00 PM
+Local time: Oct 23, 2023, 3:00 PM
+gpt-3.5-turbo-16k-0613, 45 requests
+119,494 prompt + 2,804 completion = 122,298 tokens
+1:05 PM
+Local time: Oct 23, 2023, 3:05 PM
+gpt-3.5-turbo-16k-0613, 43 requests
+121,197 prompt + 3,009 completion = 124,206 tokens
+1:10 PM
+Local time: Oct 23, 2023, 3:10 PM
+gpt-3.5-turbo-16k-0613, 13 requests
+35,452 prompt + 964 completion = 36,416 tokens
 """
 
 
