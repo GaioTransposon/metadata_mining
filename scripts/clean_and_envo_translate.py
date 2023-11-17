83b592ce6d8e8f0221ba950e30ab3fa0e7b5984f
GaioTransposon <gaio.transposon@gmail.com>
2023-11-17 17:39:04 +0100

    cpus really restricted

--- scripts/clean_and_envo_translate.py
+++ scripts/clean_and_envo_translate.py
@@ -23,6 +23,7 @@ import concurrent.futures
 import time
 import glob 
 import multiprocessing
+import traceback
 
 
 # # runs for all samples (n=3769393) in ... minutes 
@@ -37,61 +38,61 @@ import multiprocessing
 #     --ontology_dict "ontologies_dict.pkl" \
 #     --metadata_dirs "sample.info_split_dirs"
 
-def create_regex_pattern(label):
-    match = re.match(r'([a-zA-Z]+)_?(\d+)', label)
-    if not match:
-        raise ValueError(f"Unexpected label format: {label}")
-
-    prefix, digits = match.groups()
-    pattern = re.compile(prefix + r'\D+' + digits, flags=re.IGNORECASE)
-    return pattern
-
-
-    
-def process_samples(file_paths, compiled_patterns, label_info_dict, base_dir, compiled_endings):
-    log_messages = []
-    for file_path in file_paths:
-        # Extract sample name from file path for logging
-        sample = os.path.basename(file_path).replace('.txt', '')
-        log_messages.append(f"\nProcessing file {sample}...")
-        rejected_lines = []
-
-        with open(file_path, 'r') as f:
-            metadata = f.read()
-            for label, joint_info in label_info_dict.items():
-                pattern = compiled_patterns[label]  # Use the precompiled pattern
-                matches = pattern.findall(metadata)
-                if matches:
-                    # Log each successful ontology conversion
-                    for match in matches:
-                        log_messages.append(f"Converting '{label}' to '{joint_info}' in file {sample}")
-                    metadata = pattern.sub(joint_info, metadata)
-
-            cleaned_metadata_lines = []
-            for line in metadata.splitlines():
-                stripped_line = line.strip()
-                if stripped_line.lower().startswith(("experiment", "run")):
-                    continue
-                if any(compiled_end.search(stripped_line) for compiled_end in compiled_endings):
-                    rejected_lines.append(stripped_line)
-                    continue
-                cleaned_metadata_lines.append(stripped_line)
-            cleaned_metadata = "\n".join(cleaned_metadata_lines)
-
-        clean_file_name = file_path.replace('.txt', '_clean.txt')
-        with open(clean_file_name, 'w') as f:
-            f.write(cleaned_metadata)
-            f.write('\n')  # Ensure the file ends with a newline
-
-        # Log rejected lines
-        for line in rejected_lines:
-            log_messages.append(f"rejected line: {line}")
-
-    return log_messages, len(file_paths)
-
-    
-
 # =============================================================================
+# def create_regex_pattern(label):
+#     match = re.match(r'([a-zA-Z]+)_?(\d+)', label)
+#     if not match:
+#         raise ValueError(f"Unexpected label format: {label}")
+# 
+#     prefix, digits = match.groups()
+#     pattern = re.compile(prefix + r'\D+' + digits, flags=re.IGNORECASE)
+#     return pattern
+# 
+# 
+#     
+# def process_samples(file_paths, compiled_patterns, label_info_dict, base_dir, compiled_endings):
+#     log_messages = []
+#     for file_path in file_paths:
+#         # Extract sample name from file path for logging
+#         sample = os.path.basename(file_path).replace('.txt', '')
+#         log_messages.append(f"\nProcessing file {sample}...")
+#         rejected_lines = []
+# 
+#         with open(file_path, 'r') as f:
+#             metadata = f.read()
+#             for label, joint_info in label_info_dict.items():
+#                 pattern = compiled_patterns[label]  # Use the precompiled pattern
+#                 matches = pattern.findall(metadata)
+#                 if matches:
+#                     # Log each successful ontology conversion
+#                     for match in matches:
+#                         log_messages.append(f"Converting '{label}' to '{joint_info}' in file {sample}")
+#                     metadata = pattern.sub(joint_info, metadata)
+# 
+#             cleaned_metadata_lines = []
+#             for line in metadata.splitlines():
+#                 stripped_line = line.strip()
+#                 if stripped_line.lower().startswith(("experiment", "run")):
+#                     continue
+#                 if any(compiled_end.search(stripped_line) for compiled_end in compiled_endings):
+#                     rejected_lines.append(stripped_line)
+#                     continue
+#                 cleaned_metadata_lines.append(stripped_line)
+#             cleaned_metadata = "\n".join(cleaned_metadata_lines)
+# 
+#         clean_file_name = file_path.replace('.txt', '_clean.txt')
+#         with open(clean_file_name, 'w') as f:
+#             f.write(cleaned_metadata)
+#             f.write('\n')  # Ensure the file ends with a newline
+# 
+#         # Log rejected lines
+#         for line in rejected_lines:
+#             log_messages.append(f"rejected line: {line}")
+# 
+#     return log_messages, len(file_paths)
+# 
+#     
+# 
 # def process_metadata(base_dir, label_info_dict, batch_size=10):
 #     endings_to_remove = ["=$", "nan$", "not applicable$", "missing$", ". na$"]
 #     compiled_endings = [re.compile(end, re.IGNORECASE) for end in endings_to_remove]
@@ -119,9 +120,7 @@ def process_samples(file_paths, compiled_patterns, label_info_dict, base_dir, co
 #             print(f"Processed {processed_samples_count} files")
 # 
 #     return processed_samples_count
-# =============================================================================
-
-# =============================================================================
+# 
 # if __name__ == "__main__":
 #     start_time = time.time()
 # 
@@ -146,12 +145,90 @@ def process_samples(file_paths, compiled_patterns, label_info_dict, base_dir, co
 # =============================================================================
 
 
+
+
+
+
+
+import re
+import os
+import pandas as pd
+import pickle
+import datetime
+import argparse
+import concurrent.futures
+import time
+import glob
+import multiprocessing
+import traceback
+
+def create_regex_pattern(label):
+    match = re.match(r'([a-zA-Z]+)_?(\d+)', label)
+    if not match:
+        raise ValueError(f"Unexpected label format: {label}")
+
+    prefix, digits = match.groups()
+    pattern = re.compile(prefix + r'\D+' + digits, flags=re.IGNORECASE)
+    return pattern
+
+
+
+
+
+
+
+def process_samples(file_paths, compiled_patterns, label_info_dict, base_dir, compiled_endings):
+    process_id = os.getpid()  # Get the current process ID
+    log_messages = [f"Process {process_id} started processing a batch of files"]
+
+    for file_path in file_paths:
+        try:
+            sample = os.path.basename(file_path).replace('.txt', '')
+            log_messages.append(f"Processing file {sample} in Process {process_id}...")
+
+            with open(file_path, 'r') as f:
+                metadata = f.read()
+
+            for label, joint_info in label_info_dict.items():
+                pattern = compiled_patterns[label]
+                matches = pattern.findall(metadata)
+                if matches:
+                    for match in matches:
+                        log_messages.append(f"Converting '{label}' to '{joint_info}' in file {sample}")
+                    metadata = pattern.sub(joint_info, metadata)
+
+            cleaned_metadata_lines = []
+            for line in metadata.splitlines():
+                stripped_line = line.strip()
+                if stripped_line.lower().startswith(("experiment", "run")):
+                    continue
+                if any(compiled_end.search(stripped_line) for compiled_end in compiled_endings):
+                    continue
+                cleaned_metadata_lines.append(stripped_line)
+
+            cleaned_metadata = "\n".join(cleaned_metadata_lines)
+            clean_file_name = file_path.replace('.txt', '_clean.txt')
+
+            with open(clean_file_name, 'w') as f:
+                f.write(cleaned_metadata)
+                f.write('\n')
+
+        except Exception as e:
+            log_messages.append(f"Error processing file {sample}: {e}")
+            log_messages.append(traceback.format_exc())
+
+    log_messages.append(f"Process {process_id} completed processing its batch")
+    return log_messages, len(file_paths)
+
+
 def process_metadata(base_dir, label_info_dict, batch_size=10, max_workers=None):
+    
+    print(f"Max workers set to: {max_workers}")  # Print the number of max_workers
+    
     endings_to_remove = ["=$", "nan$", "not applicable$", "missing$", ". na$"]
     compiled_endings = [re.compile(end, re.IGNORECASE) for end in endings_to_remove]
     compiled_patterns = {label: create_regex_pattern(label) for label in label_info_dict.keys()}
 
-    # Gather all .txt files except *_clean.txt
     all_txt_files = glob.glob(os.path.join(base_dir, 'dirz_*', '*.txt'))
     txt_files_to_process = [file for file in all_txt_files if not file.endswith('_clean.txt')]
 
@@ -163,9 +240,8 @@ def process_metadata(base_dir, label_info_dict, batch_size=10, max_workers=None)
     log_file_path = os.path.join(base_dir, f"log_clean_and_envo_translate_{now.strftime('%Y%m%d_%H%M%S')}.txt")
     processed_samples_count = 0
 
-    # Set the number of workers if not specified
     if max_workers is None:
-        max_workers = multiprocessing.cpu_count() // 2  # Use half of the available cores
+        max_workers = multiprocessing.cpu_count() // 2
 
     with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor, open(log_file_path, 'a') as log_file:
         futures = [executor.submit(process_samples, batch, compiled_patterns, label_info_dict, base_dir, compiled_endings) for batch in sample_batches]
@@ -178,8 +254,6 @@ def process_metadata(base_dir, label_info_dict, batch_size=10, max_workers=None)
 
     return processed_samples_count
 
-
-
 if __name__ == "__main__":
     start_time = time.time()
 
@@ -189,6 +263,10 @@ if __name__ == "__main__":
     parser.add_argument('--metadata_dirs', required=True, help="Subdirectory for metadata files.")
     parser.add_argument('--max_workers', type=int, default=None, help="Maximum number of worker processes to use.")
     args = parser.parse_args()
+    
+
+    # Print out the received command line arguments for debugging
+    print(f"Received arguments: {args}")
 
     base_dir = os.path.join(args.path_to_dir, args.metadata_dirs)
     dict_file_path = os.path.join(args.path_to_dir, args.ontology_dict)
@@ -202,9 +280,5 @@ if __name__ == "__main__":
     end_time = time.time()
     total_time = end_time - start_time
     print(f"Total execution time: {total_time:.2f} seconds")
-
-
-
-
-
-
+    
+    
