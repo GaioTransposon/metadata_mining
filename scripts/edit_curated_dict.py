c274686b3e5fb98e4710bc5818053b60328e360f
GaioTransposon <gaio.transposon@gmail.com>
2023-11-22 17:55:52 +0100

    edit_cur now better interactive; confirm_geo fill in gold_dict based on extracted geo lines

--- scripts/edit_curated_dict.py
+++ scripts/edit_curated_dict.py
@@ -6,50 +6,80 @@ Created on Thu Nov  2 11:43:50 2023
 @author: dgaio
 """
 
-import pickle
 
+import pickle
 
 GOLD_DICT_PATH = "/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/gold_dict.pkl"
 
-
+# Load the existing data
 with open(GOLD_DICT_PATH, 'rb') as file:
     data, processed_pmids = pickle.load(file)
 
-
-
-# =============================================================================
-# for key, details in data.items():
-#     if len(details) > 2:
-#         print(f"Key: '{key}', Values: {details}")
-# =============================================================================
-
-
-
-
-# Define the key for which you want to change the third value
-key_to_edit = 'SRS4847591'
-new_value = 'plastic'
-value_n = 2 # 0 : PMID; 1 : biome ; 2 : sub-biome ; 3 : coordinates ; 4 : geo-loc
+# Ask the user for the sample key
+sample_key = input("What is the sample key you want to edit? ")
 
 # Check if the key exists in the dictionary
-if key_to_edit in data:
+if sample_key in data:
     # Retrieve the current tuple for the key
-    values = list(data[key_to_edit])  # Convert tuple to list to allow modifications
-    
-    print(f"The value for key '{key_to_edit}' was {values[value_n]}... ")
+    values = list(data[sample_key])  # Convert tuple to list to allow modifications
+    print(f"Current values for '{sample_key}': {values}")
 
-    values[value_n] = new_value  
-    data[key_to_edit] = tuple(values)  # Convert back to tuple and reassign to the key
-    print('\n')
-    print(data[key_to_edit])
-    
-    # Save the updated dictionary back to the .pkl file
-    with open(GOLD_DICT_PATH, 'wb') as file:
-        pickle.dump((data, processed_pmids), file)
-    
-    print(f" \n ... and has been updated to '{new_value}'.")
+    # Ask the user which value to change
+    value_position = input("Which value position do you want to change? Enter a number: ")
+    if value_position.isdigit() and 0 <= int(value_position) < len(values):
+        value_position = int(value_position)
+        print(f"Current value at position {value_position}: {values[value_position]}")
+
+        # Ask for the new value
+        new_value = input("Enter the new value: ")
+        values[value_position] = new_value
+        data[sample_key] = tuple(values)  # Convert back to tuple and reassign to the key
+
+        # Save the updated dictionary back to the .pkl file
+        with open(GOLD_DICT_PATH, 'wb') as file:
+            pickle.dump((data, processed_pmids), file)
+
+        print(f"Value at position {value_position} for '{sample_key}' has been updated to '{new_value}'.")
+    else:
+        print("Invalid position number.")
 
 else:
-    print(f"The key '{key_to_edit}' was not found in the dictionary.")
+    print(f"The key '{sample_key}' was not found in the dictionary.")
 
 
+
+
+################################################################################
+################################################################################
+
+# =============================================================================
+# # MORE DRASTIC! 
+# # to remove all 3rd and 4th values 
+# 
+# 
+# # Function to load gold data
+# def load_gold_data(filename):
+#     with open(filename, "rb") as f:
+#         return pickle.load(f)
+# 
+# # Function to save gold data
+# def save_gold_data(data, filename):
+#     with open(filename, "wb") as f:
+#         pickle.dump(data, f)
+# 
+# # Load the gold data
+# gold_data = load_gold_data(GOLD_DICT_PATH)
+# 
+# # Modify each entry in the gold_dict
+# gold_dict, processed_pmids = gold_data
+# for sample_id, details in gold_dict.items():
+#     # Check if the entry has more than 3 values and then modify it
+#     if len(details) > 3:
+#         print(details)
+#         gold_dict[sample_id] = details[:3]  # Keep only the first three values
+# 
+# # Save the modified gold data back to the pickle file
+# save_gold_data(gold_data, GOLD_DICT_PATH)
+# 
+# print("Updated gold_dict and saved to the pickle file.")
+# =============================================================================
