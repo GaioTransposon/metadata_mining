3b07f904f4179775c1f33d05eca3882958d35025
GaioTransposon <gaio.transposon@gmail.com>
2023-11-22 15:13:38 +0100

    final and working

--- scripts/clean_and_envo_translate.py
+++ scripts/clean_and_envo_translate.py
@@ -1,298 +1,153 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 24 15:08:44 2023
+Created on Mon Nov 20 17:42:04 2023
 
 @author: dgaio
 """
 
-
-# Description:  
-# goes through raw metadata files, cleans it, and 
-# replaces ontology labels at each occurrence (regex flexible)
-# with their respectective description. 
-# saves the metadata files with the same names followed by _clean before.txt 
-
-# =============================================================================
-# import re
-# import os
-# import pandas as pd
-# import pickle
-# import datetime
-# import argparse
-# import concurrent.futures
-# import time
-# import glob 
-# import multiprocessing
-# import traceback
-# 
-# 
-# # # runs for all samples (n=3769393) in ... minutes 
-# # python /Users/dgaio/github/metadata_mining/scripts/clean_and_envo_translate.py \
-# #     --path_to_dir "/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject" \
-# #     --ontology_dict "ontologies_dict.pkl" \
-# #     --metadata_dirs "sample.info_split_dirs"
-#     
-# # on atlas: 
-# # python github/metadata_mining/scripts/clean_and_envo_translate.py \
-# #     --path_to_dir "MicrobeAtlasProject" \
-# #     --ontology_dict "ontologies_dict.pkl" \
-# #     --metadata_dirs "sample.info_split_dirs"
-# 
-# def create_regex_pattern(label):
-#     match = re.match(r'([a-zA-Z]+)_?(\d+)', label)
-#     if not match:
-#         raise ValueError(f"Unexpected label format: {label}")
-# 
-#     prefix, digits = match.groups()
-#     pattern = re.compile(prefix + r'\D+' + digits, flags=re.IGNORECASE)
-#     return pattern
-#     
-# def process_samples(file_paths, compiled_patterns, label_info_dict, base_dir, compiled_endings):
-#     log_messages = []
-#     for file_path in file_paths:
-#         # Extract sample name from file path for logging
-#         sample = os.path.basename(file_path).replace('.txt', '')
-#         log_messages.append(f"\nProcessing file {sample}...")
-#         rejected_lines = []
-# 
-#         with open(file_path, 'r') as f:
-#             metadata = f.read()
-#             for label, joint_info in label_info_dict.items():
-#                 pattern = compiled_patterns[label]  # Use the precompiled pattern
-#                 matches = pattern.findall(metadata)
-#                 if matches:
-#                     # Log each successful ontology conversion
-#                     for match in matches:
-#                         log_messages.append(f"Converting '{label}' to '{joint_info}' in file {sample}")
-#                     metadata = pattern.sub(joint_info, metadata)
-# 
-#             cleaned_metadata_lines = []
-#             for line in metadata.splitlines():
-#                 stripped_line = line.strip()
-#                 if stripped_line.lower().startswith(("experiment", "run")):
-#                     continue
-#                 if any(compiled_end.search(stripped_line) for compiled_end in compiled_endings):
-#                     rejected_lines.append(stripped_line)
-#                     continue
-#                 cleaned_metadata_lines.append(stripped_line)
-#             cleaned_metadata = "\n".join(cleaned_metadata_lines)
-# 
-#         clean_file_name = file_path.replace('.txt', '_clean.txt')
-#         with open(clean_file_name, 'w') as f:
-#             f.write(cleaned_metadata)
-#             f.write('\n')  # Ensure the file ends with a newline
-# 
-#         # Log rejected lines
-#         for line in rejected_lines:
-#             log_messages.append(f"rejected line: {line}")
-# 
-#     return log_messages, len(file_paths)
-# 
-#     
-# 
-# def process_metadata(base_dir, label_info_dict, batch_size=10):
-#     endings_to_remove = ["=$", "nan$", "not applicable$", "missing$", ". na$"]
-#     compiled_endings = [re.compile(end, re.IGNORECASE) for end in endings_to_remove]
-#     compiled_patterns = {label: create_regex_pattern(label) for label in label_info_dict.keys()}
-# 
-#     # Gather all .txt files except *_clean.txt
-#     all_txt_files = glob.glob(os.path.join(base_dir, 'dirz_*', '*.txt'))
-#     txt_files_to_process = [file for file in all_txt_files if not file.endswith('_clean.txt')]
-# 
-#     print(f"Total number of files to process: {len(txt_files_to_process)}")
-#     sample_batches = [txt_files_to_process[i:i + batch_size] for i in range(0, len(txt_files_to_process), batch_size)]
-#     print(f"Total number of batches: {len(sample_batches)}")
-# 
-#     now = datetime.datetime.now()
-#     log_file_path = os.path.join(base_dir, f"log_clean_and_envo_translate_{now.strftime('%Y%m%d_%H%M%S')}.txt")
-#     processed_samples_count = 0
-# 
-#     with concurrent.futures.ProcessPoolExecutor() as executor, open(log_file_path, 'a') as log_file:
-#         futures = [executor.submit(process_samples, batch, compiled_patterns, label_info_dict, base_dir, compiled_endings) for batch in sample_batches]
-#         for future in concurrent.futures.as_completed(futures):
-#             log_messages, batch_sample_count = future.result()
-#             for message in log_messages:
-#                 log_file.write(message + "\n")
-#             processed_samples_count += batch_sample_count
-#             print(f"Processed {processed_samples_count} files")
-# 
-#     return processed_samples_count
-# 
-# if __name__ == "__main__":
-#     start_time = time.time()
-# 
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--path_to_dir', required=True, help="Path to main directory.")
-#     parser.add_argument('--ontology_dict', required=True, help="Pickle file with ontology dictionary.")
-#     parser.add_argument('--metadata_dirs', required=True, help="Subdirectory for metadata files.")
-#     args = parser.parse_args()
-# 
-#     base_dir = os.path.join(args.path_to_dir, args.metadata_dirs)
-#     dict_file_path = os.path.join(args.path_to_dir, args.ontology_dict)
-# 
-#     with open(dict_file_path, 'rb') as f:
-#         label_info_dict = pickle.load(f)
-# 
-#     processed_samples_count = process_metadata(base_dir, label_info_dict)
-# 
-#     print(f"Total processed files: {processed_samples_count}")
-#     end_time = time.time()
-#     total_time = end_time - start_time
-#     print(f"Total execution time: {total_time:.2f} seconds")
-# 
-# =============================================================================
-
-
-from multiprocessing import Queue, Process, cpu_count, Manager
-import re
 import os
-import pandas as pd
 import pickle
-import datetime
 import argparse
-import concurrent.futures
-import time
-import glob
 import multiprocessing
-import traceback
-import cProfile
+import time
+from datetime import datetime
+from multiprocessing import Process, cpu_count
 
 
-def create_regex_pattern(label):
-    match = re.match(r'([a-zA-Z]+)_?(\d+)', label)
-    if not match:
-        raise ValueError(f"Unexpected label format: {label}")
-
-    prefix, digits = match.groups()
-    pattern = re.compile(prefix + r'\D+' + digits, flags=re.IGNORECASE)
-    return pattern
-    
-
-def process_samples(file_paths, compiled_patterns, label_info_dict, base_dir, compiled_endings):
-    process_id = os.getpid()  # Get the current process ID
-    log_messages = [f"Process {process_id} started processing a batch of files"]
-
-    for file_path in file_paths:
-        try:
-            sample = os.path.basename(file_path).replace('.txt', '')
-            log_messages.append(f"Processing file {sample} in Process {process_id}...")
-
-            with open(file_path, 'r') as f:
-                metadata = f.read()
-
-            for label, joint_info in label_info_dict.items():
-                pattern = compiled_patterns[label]
-                matches = pattern.findall(metadata)
-                if matches:
-                    for match in matches:
-                        log_messages.append(f"Converting '{label}' to '{joint_info}' in file {sample}")
-                    metadata = pattern.sub(joint_info, metadata)
-
-            cleaned_metadata_lines = []
-            for line in metadata.splitlines():
-                stripped_line = line.strip()
-                if stripped_line.lower().startswith(("experiment", "run")):
-                    continue
-                if any(compiled_end.search(stripped_line) for compiled_end in compiled_endings):
-                    continue
-                cleaned_metadata_lines.append(stripped_line)
-
-            cleaned_metadata = "\n".join(cleaned_metadata_lines)
-            clean_file_name = file_path.replace('.txt', '_clean.txt')
-
-            with open(clean_file_name, 'w') as f:
-                f.write(cleaned_metadata)
-                f.write('\n')
-
-        except Exception as e:
-            log_messages.append(f"Error processing file {sample}: {e}")
-            log_messages.append(traceback.format_exc())
-
-    log_messages.append(f"Process {process_id} completed processing its batch")
-    return log_messages, len(file_paths)
-
-
-
-def process_metadata(base_dir, label_info_dict, batch_size=10, max_workers=None):
-    
-    print(f"Max workers set to: {max_workers}")  # Print the number of max_workers
-    
-    endings_to_remove = ["=$", "nan$", "not applicable$", "missing$", ". na$"]
-    compiled_endings = [re.compile(end, re.IGNORECASE) for end in endings_to_remove]
-    compiled_patterns = {label: create_regex_pattern(label) for label in label_info_dict.keys()}
-
-    all_txt_files = glob.glob(os.path.join(base_dir, 'dirz_*', '*.txt'))
-    txt_files_to_process = [file for file in all_txt_files if not file.endswith('_clean.txt')]
-
-    print(f"Total number of files to process: {len(txt_files_to_process)}")
-    sample_batches = [txt_files_to_process[i:i + batch_size] for i in range(0, len(txt_files_to_process), batch_size)]
-    print(f"Total number of batches: {len(sample_batches)}")
-
-    now = datetime.datetime.now()
-    log_file_path = os.path.join(base_dir, f"log_clean_and_envo_translate_{now.strftime('%Y%m%d_%H%M%S')}.txt")
-    processed_samples_count = 0
-
-    if max_workers is None:
-        max_workers = multiprocessing.cpu_count() // 2
-
-    with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor, open(log_file_path, 'a') as log_file:
-        futures = [executor.submit(process_samples, batch, compiled_patterns, label_info_dict, base_dir, compiled_endings) for batch in sample_batches]
-        for future in concurrent.futures.as_completed(futures):
-            log_messages, batch_sample_count = future.result()
-            for message in log_messages:
-                log_file.write(message + "\n")
-            processed_samples_count += batch_sample_count
-            print(f"Processed {processed_samples_count} files")
-
-    return processed_samples_count
-    
-    
-
-if __name__ == "__main__":
-    start_time = time.time()
-    
-    
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path_to_dir', required=True, help="Path to main directory.")
-    parser.add_argument('--ontology_dict', required=True, help="Pickle file with ontology dictionary.")
-    parser.add_argument('--metadata_dirs', required=True, help="Subdirectory for metadata files.")
-    parser.add_argument('--max_workers', type=int, default=None, help="Maximum number of worker processes to use.")
-    args = parser.parse_args()
-    
-
-    # Print out the received command line arguments for debugging
-    print(f"Received arguments: {args}")
-
-    base_dir = os.path.join(args.path_to_dir, args.metadata_dirs)
-    dict_file_path = os.path.join(args.path_to_dir, args.ontology_dict)
-
-    with open(dict_file_path, 'rb') as f:
-        label_info_dict = pickle.load(f)
-
-
-    ##
-    profiler = cProfile.Profile()
-    profiler.enable()
-    
-    processed_samples_count = process_metadata(base_dir, label_info_dict, max_workers=args.max_workers)
-    
-    profiler.disable()
-    profiler.print_stats(sort='time')
-    
-    ##
-
-    ##
-    print(f"Total processed files: {processed_samples_count}")
-    end_time = time.time()
-    total_time = end_time - start_time
-    print(f"Total execution time: {total_time:.2f} seconds")
-    
-    
-    
-# python /Users/dgaio/github/metadata_mining/scripts/clean_and_envo_translate.py \
+##############################################################################
+# # before running script, change ulimit in the session: 
+# # $ ulimit -n 200000 <-- it's an estimation derived from: 
+# # 40 (dirs and cpus at a time) * 3800 (files per dir) = 152000 --> round up: 200000
+# # then run on local: 
+# python /Users/dgaio/github/metadata_mining/scripts/clean_and_envo_translate2.py \
 #     --path_to_dir "/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject" \
 #     --ontology_dict "ontologies_dict.pkl" \
-#     --metadata_dirs "sample.info_split_dirs" --max_workers 10
-    
+#     --metadata_dirs "sample.info_split_dirs" \
+#     --max_processes 8
+##
+# # or on atlas: 
+# python github/metadata_mining/scripts/clean_and_envo_translate2.py \
+#     --path_to_dir "MicrobeAtlasProject" \
+#     --ontology_dict "ontologies_dict.pkl" \
+#     --metadata_dirs "sample.info_split_dirs" \
+#     --max_processes 40
+##############################################################################
+
+
+def modify_ontology_dict(ontology_dict):
+    modified_dict = {}
+    for term, desc in ontology_dict.items():
+        namespace, _, numeric_part = term.partition('_')
+        namespace = namespace.lower()  # Convert namespace to lower case
+        if numeric_part not in modified_dict:
+            modified_dict[numeric_part] = {}
+        modified_dict[numeric_part][namespace] = desc
+    return modified_dict
+
+
+def process_directory(dir_path, modified_ontology_dict, base_log_file_path):
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    dir_name = os.path.basename(dir_path)
+    log_file_path = f"{base_log_file_path}_log_{dir_name}_{current_time}.txt"
+
+    for file_name in os.listdir(dir_path):
+        if file_name.endswith(".txt") and not file_name.endswith("_clean.txt"):
+            process_file(os.path.join(dir_path, file_name), modified_ontology_dict, log_file_path)
+
+
+def process_file(file_path, modified_ontology_dict, log_file_path):
+    try:
+        with open(log_file_path, 'a') as log_file:
+            log_file.write(f"Processing file {os.path.basename(file_path)}...\n")
+
+        new_lines = []
+        with open(file_path, 'r') as file:
+            for line in file:
+                original_line = line  # Keep the original line for logging purposes
+                line_lower = line.lower().strip()
+                rejection_endings = ("=", "nan", "not applicable", "missing", " na", " na\n")
+
+                # Check if the line should be rejected
+                if line_lower.endswith(rejection_endings) or line_lower.startswith(("experiment", "run")):
+                    with open(log_file_path, 'a') as log_file:
+                        log_file.write(f"Rejected line: {line}")
+                    continue  # Skip the rest of the processing for this line and do not add it to new_lines
+
+                # Split line on first equals sign, if it exists
+                parts = line.split('=', 1)
+                prefix = parts[0] + '=' if len(parts) > 1 else ''
+                line_to_process = parts[-1]
+
+                for word in line_to_process.split():
+                    numeric_part = ''.join(filter(str.isdigit, word))
+                    word_lower = word.lower()  # Convert word to lower case for matching
+                    if numeric_part in modified_ontology_dict:
+                        for namespace, desc in modified_ontology_dict[numeric_part].items():
+                            if namespace in word_lower:
+                                replacement = f"'{desc}'"
+                                line_to_process = line_to_process.replace(word, replacement)
+                                with open(log_file_path, 'a') as log_file:
+                                    log_file.write(f"Converting '{word}' in line '{original_line.strip()}' to '{replacement}'\n")
+                                break  # Stop checking other namespaces for this word
+
+                new_lines.append(prefix + line_to_process)
+
+        with open(log_file_path, 'a') as log_file:
+            log_file.write("\n")
+
+        new_file_path = file_path.replace(".txt", "_clean.txt")
+        with open(new_file_path, 'w') as new_file:
+            new_file.writelines(new_lines)
+
+    except Exception as e:
+        with open(log_file_path, 'a') as log_file:
+            log_file.write(f"Error processing file {file_path}: {e}\n")
+
+
+
+def main():
+    start_time = time.time()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_to_dir", required=True)
+    parser.add_argument("--ontology_dict", required=True)
+    parser.add_argument("--metadata_dirs", required=True)
+    parser.add_argument("--max_processes", type=int, default=5, help="Maximum number of concurrent processes - these are cpus hence directories, in this case")
+    args = parser.parse_args()
+
+    ontology_dict_path = os.path.join(args.path_to_dir, args.ontology_dict)
+    with open(ontology_dict_path, 'rb') as f:
+        ontology_dict = pickle.load(f)
+
+    # Modify the ontology dictionary
+    modified_ontology_dict = modify_ontology_dict(ontology_dict)
+
+    base_dir = os.path.join(args.path_to_dir, args.metadata_dirs)
+    base_log_file_path = os.path.join(base_dir, "log_clean_and_envo_translate")
+
+    all_dir_names = [dir_name for dir_name in os.listdir(base_dir) if dir_name.startswith("dir_")]
+    max_processes = min(args.max_processes, multiprocessing.cpu_count())
+
+    for i in range(0, len(all_dir_names), max_processes):
+        processes = []
+        for dir_name in all_dir_names[i:i + max_processes]:
+            dir_path = os.path.join(base_dir, dir_name)
+            p = multiprocessing.Process(target=process_directory, args=(dir_path, modified_ontology_dict, base_log_file_path))
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join()  # Wait for the current batch of processes to complete before starting the next batch
+
+    end_time = time.time()
+    print(f"Script executed in {end_time - start_time:.2f} seconds")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
