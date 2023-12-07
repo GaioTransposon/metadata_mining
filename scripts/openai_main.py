58d5d8a5767ae9bab737382cec5426020ad84027
GaioTransposon <gaio.transposon@gmail.com>
2023-12-07 17:36:45 +0100

    tokenizer method passed as arg

--- scripts/openai_main.py
+++ scripts/openai_main.py
@@ -29,6 +29,7 @@ def parse_arguments():
     parser.add_argument('--seed', type=int, required=True, help='choose a seed for the random shuffling of the samples e.g.: 42')
     parser.add_argument('--directory_with_split_metadata', type=str, required=True, help='Directory with split metadata')
     parser.add_argument('--system_prompt_file', type=str, required=True, help='it should be named openai_system_prompt.txt (location: work_dir)')
+    parser.add_argument('--encoding_name', type=str, required=True, help='name of encoder (for tokenizer) e.g.: cl100k_base')
     parser.add_argument('--api_key_path', type=str, required=True, help='Path to the OpenAI API key')
     parser.add_argument('--model', type=str, required=True, help='GPT model to use')
     parser.add_argument('--temperature', type=float, required=True, help='Temperature setting for the GPT model')
@@ -45,13 +46,13 @@ def main():
     args = parse_arguments()  # Assumes the parse_arguments function is defined at the top level
 
     # Phase 1: Metadata Processing
-    metadata_processor = MetadataProcessor(args.work_dir, args.input_gold_dict, args.n_samples_per_biome, args.chunk_size, args.system_prompt_file, args.seed, args.directory_with_split_metadata)
+    metadata_processor = MetadataProcessor(args.work_dir, args.input_gold_dict, args.n_samples_per_biome, args.chunk_size, args.system_prompt_file, args.encoding_name, args.seed, args.directory_with_split_metadata)
     
     
     chunks = metadata_processor.run()
 
     # Phase 2: GPT Interaction
-    gpt_interactor = GPTInteractor(args.work_dir, args.n_samples_per_biome, args.chunk_size, args.system_prompt_file, args.api_key_path, args.model, args.temperature, args.max_tokens, args.top_p, args.frequency_penalty, args.presence_penalty)
+    gpt_interactor = GPTInteractor(args.work_dir, args.n_samples_per_biome, args.chunk_size, args.system_prompt_file, args.encoding_name, args.api_key_path, args.model, args.temperature, args.max_tokens, args.top_p, args.frequency_penalty, args.presence_penalty)
     gpt_interactor.run(chunks)
 
     # Phase 3: Parsing GPT Output
@@ -85,6 +86,7 @@ if __name__ == "__main__":
 #     --seed 42 \
 #     --directory_with_split_metadata "sample.info_split_dirs" \
 #     --system_prompt_file "openai_system_prompt.txt" \
+#     --encoding_name "cl100k_base" \
 #     --api_key_path "/Users/dgaio/my_api_key" \
 #     --model "gpt-3.5-turbo-1106" \
 #     --temperature 1.00 \
@@ -92,5 +94,4 @@ if __name__ == "__main__":
 #     --top_p 0.75 \
 #     --frequency_penalty 0.25 \
 #     --presence_penalty 1.5
-
 
