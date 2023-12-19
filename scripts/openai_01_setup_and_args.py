69a92298650028d1bf4d92ea868ddd8c12e44335
GaioTransposon <gaio.transposon@gmail.com>
2023-12-19 14:35:56 +0100

    edited comments

--- scripts/openai_01_setup_and_args.py
+++ scripts/openai_01_setup_and_args.py
@@ -17,41 +17,52 @@ import logging
 # PHASE 0: set up a logging system 
 # =======================================================
 
-# Define a logging function that logs to both the console and a file
 
 class CustomFormatter(logging.Formatter):
-    MAX_LENGTH = 250  # Set this to your desired length
+    MAX_LENGTH = 500  # log messages longer than this # of characters, will be truncated 
 
     def format(self, record):
         if record.levelno == logging.DEBUG and len(record.msg) > self.MAX_LENGTH:
             record.msg = record.msg[:self.MAX_LENGTH] + "..."
         return super().format(record)
     
-
+# Logging function: logs to both the console and a file
 def setup_logging():
-    # get dir of script
+    
+    # determine the dir of the running script - logging file will be save there
     script_directory = os.path.dirname(os.path.abspath(__file__))
     
     # give timestamp to log filename 
     log_filename = datetime.now().strftime("openai_validate_biomes_%Y%m%d_%H%M%S.log")
     
-    # Join the directory with the log filename to get the full path
+    # construct full path
     log_filepath = os.path.join(script_directory, log_filename)
 
-    # Create a logger
+    # create logger
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)  # Set the logger level to DEBUG to capture all types of logs
+    logger.setLevel(logging.DEBUG)  # captures all logs at the DEBUG level and above
 
     formatter = CustomFormatter('%(asctime)s [%(levelname)s]: %(message)s')
 
-    # File handler for logging
+
+    ###
+    # sets up logging to file and to console with different log levels, but a common formatter.
+
+    # file handler for logging
     file_handler = logging.FileHandler(log_filepath)
-    file_handler.setLevel(logging.INFO)  # Set this to INFO to capture INFO and higher level logs
+    file_handler.setLevel(logging.INFO)  # captures INFO level and higher logs (i.e., WARNING, ERROR, CRITICAL)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
-    # Console handler for logging
+    # console handler for logging
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.WARNING)  # Set this to WARNING to capture only WARNING and higher level logs
+    console_handler.setLevel(logging.WARNING)  # captures WARNING level and higher logs (i.e., ERROR, CRITICAL)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
+    ###
+    
+
+
+
+    
+    
