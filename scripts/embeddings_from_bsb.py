589fc3c2c26a602ee6457966ea9c0636681fe02e
GaioTransposon <gaio.transposon@gmail.com>
2024-05-30 16:18:39 +0200

    gets embeddings for gpt output file snad gold dict

--- scripts/embeddings_from_bsb.py
+++ scripts/embeddings_from_bsb.py
@@ -6,144 +6,136 @@ Created on Tue May 28 15:40:32 2024
 @author: dgaio
 """
 
-import os
-import json
-import openai
-import numpy as np
-import pickle
 import csv
-import time
-from datetime import datetime
+import openai
+import json
+import glob
+import os
+import pickle
 
 
+directory_path = '/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject'
+output_dir = os.path.join(directory_path, 'embeddings')
+api_key_path = '/Users/dgaio/my_api_key_embeddings'
+gold_dict_path = '/Users/dgaio/github/metadata_mining/source_data/gold_dict.pkl'
+input_file_pattern = 'gpt_clean_output*.csv'
 
-def get_embeddings(data_dict, verbose='no'):
+
+# loads ground truth and combines for each sample, biome with sub-biome
+def load_and_combine_gold_dict(gold_dict_path):
+    gold_dict_bsb = {}
+    with open(gold_dict_path, 'rb') as file:
+        data = pickle.load(file)
+        for key, values in data.items():
+            if len(values) >= 3:
+                combined_text = f"{values[1]} - {values[2]}"
+                #print(key)  
+                #print(combined_text)  
+                gold_dict_bsb[key] = combined_text
+    return gold_dict_bsb
+
+
+# get embeddings in batches (it's faster)
+def get_embeddings(data_dict):
     embeddings_dict = {}
     failed_samples = []
     sample_ids = list(data_dict.keys())
     descriptions = list(data_dict.values())
 
-    try:
-        start_api_call_time = time.time()
-        batch_size = 20
-        for i in range(0, len(descriptions), batch_size):
-            chunk = descriptions[i:i + batch_size]
-            sample_ids_chunk = sample_ids[i:i + batch_size]
-            try:
-                response = openai.Embedding.create(input=chunk, engine="text-embedding-ada-002")
-                embeddings = [embedding['embedding'] for embedding in response['data']]
-                for j, sample_id in enumerate(sample_ids_chunk):
-                    embeddings_dict[sample_id] = np.array(embeddings[j], dtype=np.float32)
-            except openai.APIError as e:
-                print(f"API error for batch starting with {sample_ids_chunk[0]}: {e}")
-                failed_samples.extend(sample_ids_chunk)
-            except Exception as e:
-                print(f"Unexpected error for batch starting with {sample_ids_chunk[0]}: {e}")
-                failed_samples.extend(sample_ids_chunk)
+    batch_size = 20
+    for i in range(0, len(descriptions), batch_size):
+        chunk = descriptions[i:i + batch_size]
+        sample_ids_chunk = sample_ids[i:i + batch_size]
+        try:
+            response = openai.Embedding.create(input=chunk, engine="text-embedding-ada-002")
+            embeddings = [embedding['embedding'] for embedding in response['data']]
+            for j, sample_id in enumerate(sample_ids_chunk):
+                embeddings_dict[sample_id] = embeddings[j]
+        except Exception as e:
+            print(f"Failed to retrieve embeddings for {sample_ids_chunk[0]} to {sample_ids_chunk[-1]}: {e}")
+            failed_samples.extend(sample_ids_chunk)
 
-        end_api_call_time = time.time()
-        if verbose.lower() == 'yes':
-            print(f"API call for {len(descriptions)} texts took {end_api_call_time - start_api_call_time:.2f} seconds")
-    except Exception as e:
-        print(f"General error in embedding function: {e}")
-
-    return embeddings_dict, failed_samples
+    return embeddings_dict, failed_samples, output_file_path
 
 
+# opens gpt output files, gets biome and sub-biome, and gets embeddings for those (for each sample)
+def process_file(csv_file_path):
+    samples = {}
+    with open(csv_file_path, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)  # skip header 
+        for row in reader:
+            # skip files taht dont have at leats 5 columns
+            if len(row) < 5:
+                continue  
+            sample_id = row[0]
+            combined_text = f"{row[1]} - {row[4]}"
+            samples[sample_id] = combined_text
 
+    embeddings_dict, failed_samples = get_embeddings(samples)
 
+    # output filename based on input filename
+    base_filename = os.path.basename(csv_file_path)
+    output_filename = base_filename.replace('.csv', '_bsbembeddings.json')
+    output_file_path = os.path.join(output_dir, output_filename)
 
-def load_data(source, source_type):
-    if source_type == 'pickle':
-        with open(source, 'rb') as file:
-            return pickle.load(file)
-    elif source_type == 'csv':
-        with open(source, mode='r', newline='') as file:
-            reader = csv.DictReader(file)
-            if 'col_4' not in reader.fieldnames:
-                print(f"Warning: 'col_4' not found in: \n {source}. \nSkipping this file.\n")
-                return None
-            return {row['col_0']: f"{row['col_1']} - {row['col_4']}" for row in reader if 'col_4' in row}
-    else:
-        raise ValueError("Unsupported source type")
+    return embeddings_dict, failed_samples, output_file_path
 
 
 
-# =============================================================================
-# def process_file(file_path, output_dir):
-#     filename = os.path.basename(file_path)
-#     embedding_filename = f"{filename.split('.')[0]}_bsbembeddings.json"
-#     embedding_filepath = os.path.join(output_dir, embedding_filename)
-# 
-#     if os.path.exists(embedding_filepath):
-#         print(f"Embedding file already exists: {embedding_filepath}")
-#         return
-# 
-#     # Assuming the source type from the file extension
-#     source_type = 'pickle' if file_path.endswith('gold_dict.pkl') else 'csv'
-#     
-#     data_dict = load_data(file_path, source_type)
-#     embeddings, _ = get_embeddings(data_dict)
-#     
-#     with open(embedding_filepath, 'w') as f:
-#         json.dump(embeddings, f)
-#     print(f"Saved new embeddings to {embedding_filepath}")
-# =============================================================================
-
-def process_file(file_path, output_dir):
-    filename = os.path.basename(file_path)
-    embedding_filename = f"{filename.split('.')[0]}_bsbembeddings.json"
-    embedding_filepath = os.path.join(output_dir, embedding_filename)
-
-    if os.path.exists(embedding_filepath):
-        print(f"Embedding file already exists: {embedding_filepath}")
-        return
-
-    # Assuming the source type from the file extension
-    source_type = 'pickle' if file_path.endswith('.pkl') else 'csv'
-    data_dict = load_data(file_path, source_type)
-
-    if data_dict is None:  # Check if data_dict is not empty
-        #print(f"No valid data found in {file_path}. Skipping embedding generation.")
-        return
-
-    embeddings, failed_samples = get_embeddings(data_dict)
-
-    with open(embedding_filepath, 'w') as f:
-        json.dump(embeddings, f)
-    print(f"Saved new embeddings to {embedding_filepath}")
+def save_embeddings(embeddings_dict, failed_samples, output_file_path):
+    with open(output_file_path, 'w') as json_file:
+        json.dump({'embeddings': embeddings_dict, 'failed_samples': failed_samples}, json_file)
 
 
 
-def main():
-    directory = '/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject'
-    output_dir = '/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/embeddings'
-    os.makedirs(output_dir, exist_ok=True)
-
-    # Initialize API key
-    with open('/Users/dgaio/my_api_key_embeddings', "r") as file:
-        openai.api_key = file.read().strip()
+with open(api_key_path, "r") as file:
+    openai.api_key = file.read().strip()
 
 
-    # List for gpt_clean_output files
-    files_to_process = [os.path.join(directory, f) for f in os.listdir(directory) if f.startswith('gpt_clean_output')]
 
-    # Add specific path for gold_dict.pkl
-    gold_dict_path = "/Users/dgaio/github/metadata_mining/source_data/gold_dict.pkl"
-    files_to_process.append(gold_dict_path)  # Add the gold_dict path to the list
 
-    # for testing: 
-    files_to_process = files_to_process[:100]
+############
+# Get embeddings from gpt output files: 
+file_paths = glob.glob(os.path.join(directory_path, input_file_pattern))
+existing_output_files = set(os.path.basename(f) for f in glob.glob(os.path.join(output_dir, '*_bsbembeddings.json')))
+# processed_files = 0
+
+for file_path in file_paths:
+    base_filename = os.path.basename(file_path)
+    expected_output_filename = base_filename.replace('.csv', '_bsbembeddings.json')
     
-    for file_path in files_to_process:
-        process_file(file_path, output_dir)
+    if expected_output_filename in existing_output_files:
         
-    print(len(files_to_process))
+        print('File:\n', expected_output_filename, '\nexists')
+        continue  
+
+    # for testing purposes: 
+    # if processed_files >= 2:
+    #     break
+    
+    print('\nGetting embeddings for:\n', file_path)
+    process_file(file_path)
+    # processed_files += 1
+
+# print(f"Processed {processed_files} files.")
+############
 
 
 
-if __name__ == "__main__":
-    main()
+############
+# Get embeddings from ground truth: 
+gold_dict_bsb = load_and_combine_gold_dict(gold_dict_path)
+output_filename = 'gold_dict_bsbembeddings.json'
+output_file_path = os.path.join(output_dir, output_filename)
+
+if os.path.exists(output_file_path):
+    print('Gold dict embeddings file already exists:', output_file_path)
+else:
+    embeddings_dict, failed_samples = get_embeddings(gold_dict_bsb)
+    save_embeddings(embeddings_dict, failed_samples, output_file_path)
+    print(f"Embeddings for gold dict saved to {output_file_path}.")
+############
 
 
 
@@ -152,6 +144,4 @@ if __name__ == "__main__":
 
 
 
-
-
-
+    
