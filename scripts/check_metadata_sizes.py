fd29448234b10115bfdfc21c82d894e88af1ea7c
GaioTransposon <gaio.transposon@gmail.com>
2024-07-26 21:08:34 +0200

    time run

--- scripts/check_metadata_sizes.py
+++ scripts/check_metadata_sizes.py
@@ -10,6 +10,14 @@ Created on Wed Jun  5 16:11:19 2024
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import tiktoken
+import numpy as np
+from sklearn.utils import resample
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+# PART 1: Overall look at metadata files size distribution: 
+
 
 base_dir = "/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/sample.info_split_dirs/"
 
@@ -36,6 +44,8 @@ df = df.sort_values(by="file_size", ascending=False) #.head(10000)
 summary_stats = df.describe()
 print(summary_stats)
 
+
+
 # Boxplot distribution
 plt.figure(figsize=(10, 5))
 plt.boxplot(df['file_size'], vert=False)
@@ -43,14 +53,6 @@ plt.title('File sizes')
 plt.xlabel('file size (bytes)')
 plt.grid(True)
 plt.show()
-
-
-
-# /Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/sample.info_split_dirs/dir_493/ERS810493_clean.txt	
-# file size: 15373
-# according to gpt tokenizer: 
-# Tokens: 8,008
-# Characters: 15372
 
 
 # Visualizing majority: 
@@ -89,17 +91,9 @@ plt.show()
 
 
 
+# PART 2: Analysis of reduction based on file size (bytes) reduction: 
 
 
-
-
-
-
-
-
-import os
-import pandas as pd
-import matplotlib.pyplot as plt
 
 base_dir = "/Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/sample.info_split_dirs/"
 
@@ -122,6 +116,10 @@ for dirpath, dirnames, filenames in os.walk(base_dir):
 original_df = pd.DataFrame(original_files_data)
 clean_df = pd.DataFrame(clean_files_data)
 
+# remove log files
+original_df = original_df[~original_df['file_path'].str.contains(r'/log')]
+clean_df = clean_df[~clean_df['file_path'].str.contains(r'/log')]
+
 
 # Sort DataFrames by file size
 original_df_sorted = original_df.sort_values(by='file_size').reset_index(drop=True)
@@ -131,21 +129,97 @@ clean_df_sorted = clean_df.sort_values(by='file_size').reset_index(drop=True)
 median_index_original = len(original_df_sorted) // 2
 median_index_clean = len(clean_df_sorted) // 2
 
-median_file_original = original_df_sorted.loc[median_index_original, 'file_path']
-median_file_clean = clean_df_sorted.loc[median_index_clean, 'file_path']
 
-print(f"Median file path for *.txt files: {median_file_original}")
-# /Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/sample.info_split_dirs/dir_776/SRS3616776.txt <-- 1877 chars; 564 tokens
-print(f"Median file path for *clean.txt files: {median_file_clean}")
-# /Users/dgaio/cloudstor/Gaio/MicrobeAtlasProject/sample.info_split_dirs/dir_890/SRS6434890_clean.txt <-- 1133 chars; 278 tokens
-
-# Optional: Display total size reduction
 total_size_original = original_df['file_size'].sum()
 total_size_clean = clean_df['file_size'].sum()
-total_reduction = total_size_original - total_size_clean
+
+# Calculate the absolute reduction in bytes
+total_reduction_bytes = total_size_original - total_size_clean
+
+# Calculate the percentage reduction
+total_reduction_percentage = (total_reduction_bytes / total_size_original) * 100
+
 print(f"Total original size: {total_size_original} bytes")
 print(f"Total clean size: {total_size_clean} bytes")
-print(f"Total reduction in size: {total_size_clean/total_size_original*100} bytes")
+print(f"Total reduction in size: {total_reduction_bytes} bytes") 
+print(f"Percentage reduction in size: {total_reduction_percentage:.2f}%")  # Percentage reduction in size: 34.53%
+
+
+
+
+# PART 3: Analysis of reduction based on tokens reduction: 
+
+
+# Initialize the tokenizer
+enc = tiktoken.get_encoding("cl100k_base")
+
+# Function to read file content and count tokens
+def read_and_tokenize(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    tokens = enc.encode(content)
+    return len(tokens)
+
+    
+# Function to perform bootstrap sampling from Q1 to Q3 and calculate token reduction
+def bootstrap_token_reduction_iqr(original_df, clean_df, n_iterations=100, sample_size=100):
+    reductions = []
+    original_token_counts = []
+    clean_token_counts = []
+    for _ in range(n_iterations):
+        # Sample with replacement from Q1 to Q3 of the original and cleaned dataframes
+        original_sample = resample(original_df[(original_df['file_size'] >= original_df['file_size'].quantile(0.25)) & 
+                                               (original_df['file_size'] <= original_df['file_size'].quantile(0.75))],
+                                   n_samples=sample_size, replace=True)
+        clean_sample = resample(clean_df[(clean_df['file_size'] >= clean_df['file_size'].quantile(0.25)) & 
+                                         (clean_df['file_size'] <= clean_df['file_size'].quantile(0.75))],
+                                n_samples=sample_size, replace=True)
+
+        # Calculate the mean token counts for each sample
+        original_tokens = np.array([read_and_tokenize(path) for path in original_sample['file_path']])
+        clean_tokens = np.array([read_and_tokenize(path) for path in clean_sample['file_path']])
+        original_mean_tokens = np.mean(original_tokens)
+        clean_mean_tokens = np.mean(clean_tokens)
+        
+        # Store the mean token counts
+        original_token_counts.append(original_mean_tokens)
+        clean_token_counts.append(clean_mean_tokens)
+        
+        # Calculate the reduction in tokens
+        token_reduction = original_mean_tokens - clean_mean_tokens
+        percentage_reduction = (token_reduction / original_mean_tokens) * 100
+        reductions.append(percentage_reduction)
+
+    return reductions, np.mean(original_token_counts), np.mean(clean_token_counts)
+
+
+
+# Perform bootstrap analysis
+results, avg_original_tokens, avg_clean_tokens = bootstrap_token_reduction_iqr(original_df, clean_df)
+
+# Calculate mean and 95% confidence interval
+mean_reduction = np.mean(results)
+ci_lower, ci_upper = np.percentile(results, [2.5, 97.5])
+
+print(f"Estimated token reduction percentage: {mean_reduction:.2f}%")
+print(f"95% Confidence Interval: [{ci_lower:.2f}%, {ci_upper:.2f}%]")
+
+# # run with n_iterations 100, sample_size=100
+# Estimated token reduction percentage: 35.84%
+# 95% Confidence Interval: [30.46%, 40.38%]
+# # again: 
+# Estimated token reduction percentage: 36.32%
+# 95% Confidence Interval: [31.18%, 40.60%]
+# # again: 
+# Estimated token reduction percentage: 35.91%
+# 95% Confidence Interval: [31.56%, 41.52%]
+
+
+print(f"Average original tokens: {avg_original_tokens:.2f}")
+print(f"Average cleaned tokens: {avg_clean_tokens:.2f}")
+
+# 535.4 tokens * 3.8 = 2034444000 --> 2034444000*0.5/1000000 = $1017.222 input cost without cleaning
+# 340.06 tokens * 3.8 = 1292228000 --> 1292228000*0.5/1000000 = $646.114 input cost after cleaning
 
 
 
@@ -155,26 +229,15 @@ print(f"Total reduction in size: {total_size_clean/total_size_original*100} byte
 
 
 
-# bash approach 
-
-base_dir="/mnt/mnemo5/dgaio/MicrobeAtlasProject/sample.info_split_dirs/dir_*"
 
 
-# median txt file (excl. clean): 
-find $base_dir -type f -name "*.txt" ! -name "*clean.txt" -exec du -k {} + | sort -n -k1 > sorted_files.txt
-num_files=$(cat sorted_files.txt | wc -l)
-median_index=$(( (num_files + 1) / 2 ))
-median_file=$(awk "NR==$median_index {print \$2}" sorted_files.txt)
-echo "The file with the median size is: $median_file"
-# The file with the median size is: /mnt/mnemo5/dgaio/MicrobeAtlasProject/sample.info_split_dirs/dir_607/SRS5604607.txt
-# cat /mnt/mnemo5/dgaio/MicrobeAtlasProject/sample.info_split_dirs/dir_607/SRS5604607.txt
-# Tokens 305 Characters 1141
 
 
-# median clean file: 
-find $base_dir -type f -name "*clean.txt" -exec du -k {} + | sort -n -k1 > sorted_clean_files.txt
-num_files=$(cat sorted_files.txt | wc -l)
-median_index=$(( (num_files + 1) / 2 ))
-median_file=$(awk "NR==$median_index {print \$2}" sorted_clean_files.txt)
-echo "The file with the median size is: $median_file"
+
+
+
+
+
+
+
 
